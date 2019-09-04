aff09de4f29e89a55c22ad7b74a300049b9988cb
Rener Oliveira <rener.oliveira@fgv.edu.br>
2019-09-04 20:38:36 -0300

    criando classe pessoas e definindo atributos

--- Pessoa.py
+++ Pessoa.py
@@ -1,8 +1,14 @@
 import random
 class Pessoa:
-    def __init__(self, tipo_grad,rede_ensino):
+    def __init__(self, tipo_grad,rede_ensino,modalidade,dif_frequencia,sexo,cor,rendimento_percapita,):
         self.tipo_grad = tipo_grad
         self.rede_ensino = rede_ensino
+        self.modalidade = modalidade
+        self.dif_frequencia = dif_frequencia
+        self.sexo = sexo
+        self.cor = cor
+        self.rendimento_percapita = rendimento_percapita
+
 
 p1=Pessoa("não superior")
 print(p1.tipo_grad)
