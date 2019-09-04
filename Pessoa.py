24115f6eeca8c1ae5d4c9fd499d6371ee1fc277e
Rener Oliveira <rener.oliveira@fgv.edu.br>
2019-09-04 19:21:44 -0300

    redefinindo pessoas

--- Pessoa.py
+++ Pessoa.py
@@ -1,16 +1,11 @@
 import random
 class Pessoa:
-    def __init__(self, tipo_grad):
+    def __init__(self, tipo_grad,rede_ensino):
         self.tipo_grad = tipo_grad
+        self.rede_ensino = rede_ensino
 
-    def tipo_grad():
-        i=random.uniform(0, 1)
-        if i<6.6/100:
-            return 'superior de tecnologia'
-        else:
-            return 'não superior de tecnologia'
-
-
+p1=Pessoa("não superior")
+print(p1.tipo_grad)
 
 
 
