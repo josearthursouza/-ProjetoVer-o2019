dd8c39ddd09e6ff26049949253e260e12a274bc1
José Arthur <53868368+josearthursouza@users.noreply.github.com>
2019-09-04 19:28:36 -0300

    Update Populacao.py

--- Populacao.py
+++ Populacao.py
@@ -1,10 +1,22 @@
+import numpy as np
+import random
+import Pessoa
 class Populacao:
     def _init_(self, tamanho=1000):
         self.tamanho = tamanho
         self.individuos = []
     def amostra(self, n):
         pass
+    def tipo_grad():
+        j=random.uniform(477-((3.7/100)*7242),477+((3.7/100)*7242))
+        i=random.uniform(6765-((0.3/100)*7242),6765+((0.3/100)*7242))
+        k=7242+i+j
+        m=random.uniform(0,k)
+        if m<=j:
+            return 'superior de tecnologia'
+        else:
+            return 'não superior de tecnologia'
 
-p1=Pessoa(Pessoa.tipo_grad())
+#print("testando push de novo")
+p1=Pessoa.Pessoa(Populacao.tipo_grad())
 print(p1.tipo_grad)
-print("testando push de novo")
