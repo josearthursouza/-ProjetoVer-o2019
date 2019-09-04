e17baf651df6dd797c95de6ff0f739b7312df0f3
José Arthur de Souza <B39334@fgv.edu.br>
2019-09-04 18:29:39 -0300

    nova classe criada

--- Pessoa.py
+++ Pessoa.py
@@ -12,13 +12,8 @@ class Pessoa:
 
 
 
-class Populacao:
-    def _init_(self, tamanho=1000):
-        self.tamanho = tamanho
-        self.individuos = []
-    def amostra(self, n):
-        pass
 
-p1=Pessoa(Pessoa.tipo_grad())
-print(p1.tipo_grad)
-print("testando push")
+
+
+
+
